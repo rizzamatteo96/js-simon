3847284ebf6902f871fdb76044fed46031bfa6a2
rizzamatteo96 <77894068+rizzamatteo96@users.noreply.github.com>
2021-06-11 15:46:21 +0200

    init project

---
+++ js/script.js
@@ -0,0 +1,28 @@
+//TODO Un alert() espone 5 numeri generati casualmente.
+//TODO Da li parte un timer di 30 secondi.
+//TODO Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+//TODO Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//! ------Sezione per le funzioni------
