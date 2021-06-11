d36876197aed90dca853ec95cd618768f251990f
rizzamatteo96 <77894068+rizzamatteo96@users.noreply.github.com>
2021-06-11 15:52:28 +0200

    add cinque numeri diversi tra loro random

--- js/script.js
+++ js/script.js
@@ -3,6 +3,20 @@
 //TODO Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 //TODO Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
+//* dichiaro le variabili globali principali
+var arrayRndNum = [];
+var quantityNum = 5;
+var numMin = 1;
+var numMax = 100;
+
+//* creo n numeri casuali (n = quantityNum)
+while (arrayRndNum.length < quantityNum){
+    var numero = rndNum(numMin,numMax);
+    if(!arrayRndNum.includes(numero)){
+        arrayRndNum.push(numero);
+    }
+}
+console.log(arrayRndNum);
 
 
 
@@ -26,3 +40,6 @@
 
 
 //! ------Sezione per le funzioni------
+function rndNum(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
