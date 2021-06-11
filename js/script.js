87b0b73fd5d940e83b904e25ac815b79b65bb630
rizzamatteo96 <77894068+rizzamatteo96@users.noreply.github.com>
2021-06-11 16:20:39 +0200

    Finish project add countDown function add game function add result function

--- js/script.js
+++ js/script.js
@@ -16,30 +16,47 @@ while (arrayRndNum.length < quantityNum){
         arrayRndNum.push(numero);
     }
 }
-console.log(arrayRndNum);
+alert('Questi sono i numeri che ti devi ricordare: ' + arrayRndNum);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+var time = 30;
+var countDown = setInterval(function(){
+    console.log(time);
+    time--;
+    if(!time){
+        clearInterval(countDown);
+        game();
+    }
+}, 1000);
 
 
 //! ------Sezione per le funzioni------
 function rndNum(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+function game(){
+    var tentativi = quantityNum;
+    var numeriUser = [];
+    for(var i = tentativi; i > 0; i--){
+        var numCheck = parseInt(prompt('Inserisci uno alla volta i numeri che ricordi: tentativi rimasti = ' + i));
+        if(numeriUser.includes(numCheck)){
+            alert('Attenzione: il numero "' + numCheck + '" era già stato inserito. Hai perso un tentativo!');
+        }
+        else if(isNaN(numCheck)){
+            alert('Attenzione: il valore inserito NON è un numero. Hai perso un tentativo!');
+        }
+        else if(arrayRndNum.includes(numCheck)){
+            numeriUser.push(numCheck);
+        }
+    }
+
+    result(numeriUser);
+}
+
+function result(arrayWin){
+
+    console.log('Sei riuscito a trovare ' + arrayWin.length + ' numeri su ' + quantityNum);
+    console.log('I numeri da te trovati sono i seguenti: ' + arrayWin);
+
 }
